f6f4f944f608f402d97b00c5f0327090bc318248
Mitch Currie <mitchell.jc@hotmail.com>
2025-05-17 13:52:36 +1200

    Add character controller script

---
+++ Box Sorting/Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    public void Move(Vector2 direction)
+    {
+        
+    }
+
+    public void StopMoving()
+    {
+        
+    }
+
+    private void PickUpBox()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
