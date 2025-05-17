cc4829e400fe4901e3027df57433d6fbb64bd521
Mitch Currie <mitchell.jc@hotmail.com>
2025-05-17 23:45:17 +1200

    Add character controller functionality

--- Box Sorting/Assets/Scripts/Character/CharacterController.cs
+++ Box Sorting/Assets/Scripts/Character/CharacterController.cs
@@ -1,15 +1,32 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class CharacterController : MonoBehaviour
 {
+    private static readonly int WALK_ANIM_BOOL = Animator.StringToHash("Walk");
+    private static readonly int HOLD_ANIM_BOOL = Animator.StringToHash("Hold");
+
+    [Header("Settings")]
     [SerializeField] private float _movementSpeed;
+    
+    [Header("State Machine")]
+    [SerializeField] private State _defaultState;
     [SerializeField] private State[] _states;
 
     private StateMachine _stateMachine;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Vector2 _targetPosition;
     private bool _holdingBox;
+    private bool _moving;
 
     private void Awake()
     {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
         InitStateMachine();
     }
 
@@ -21,16 +38,32 @@ public class CharacterController : MonoBehaviour
         {
             _stateMachine.Add(state); // add any other relevant SO data in here
         }
+        
+        _stateMachine.TryChangeState(_defaultState.Name);
     }
 
-    public void Move(Vector2 direction)
+    private void Update()
     {
-        
+        _stateMachine.Update(Time.deltaTime);
     }
 
-    public void StopMoving()
+    private void FixedUpdate()
     {
+        if (!_moving)
+        {
+            return;
+        }
         
+        transform.position = Vector2.MoveTowards(transform.position, _targetPosition, 
+            Time.deltaTime * _movementSpeed);
+    }
+
+    public void MoveTo(Vector2 direction, Vector2 targetPosition)
+    {
+        _spriteRenderer.flipX = direction == Vector2.left;
+        _targetPosition = targetPosition;
+        _moving = true;
+        _animator.SetBool(WALK_ANIM_BOOL, true);
     }
 
     private void PickUpBox()
@@ -40,6 +73,7 @@ public class CharacterController : MonoBehaviour
             return;
         }
         
+        _animator.SetBool(HOLD_ANIM_BOOL, true);
         _holdingBox = true;
     }
     
@@ -50,6 +84,8 @@ public class CharacterController : MonoBehaviour
             return;
         }
         
+        _animator.SetBool(HOLD_ANIM_BOOL, false);
+        _animator.SetBool(WALK_ANIM_BOOL, false);
         _holdingBox = false;
     }
 
@@ -57,7 +93,7 @@ public class CharacterController : MonoBehaviour
     {
         if (collision.CompareTag("Box"))
         {
-            Destroy(collision.gameObject);
+            
         }
     }
 }
